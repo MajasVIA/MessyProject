57ae509b33608d8680f2088b321e9efeea4ab41d
missy1020 <82297498+missy1020@users.noreply.github.com>
2025-11-20 16:36:48 +0100

    fixed code in User

--- src/User.java
+++ src/User.java
@@ -1,43 +1,16 @@
 public class User
 {
-    public int c_id;
-    public String c_nam;
-    public String address;
+    private final String c_nam;
+    private final int c_id;
 
-    public User(int i, String n){
+    public User(String n, int i)
+    {
+        c_nam = n;
         c_id = i;
-        if(n.length() < 0 || n.length() > 100) // Testing length
-        {
-            return; //If string is invalid, we dont try to store it
-        }
-        c_nam = n;
     }
 
-    public void setC_nam(String n)
+    public String getName()
     {
-        if(n.length() < 0)
-        {
-            return; //If string is invalid, we dont try to store it
-        }
-        c_nam = n;
-    }
-
-    public void setA(String a)
-    {
-        a = a;
-    }
-
-    public int geti(){ return c_id; }
-
-    public String getn(){ return c_nam; }
-
-    public String geta(){ return "STREETNAME"; }
-
-    @Override
-    public boolean equals(Object o){
-        if(o instanceof User u){
-            return u.c_nam == c_nam;
-        }
-        return false;
+        return c_nam;
     }
 }
